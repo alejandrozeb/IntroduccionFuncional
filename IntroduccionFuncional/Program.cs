12d7e6de5b4def792b66cf5f159ffc2fb945075f
unknown <alejandrojzeballos@gmail.com>
2021-08-02 18:09:06 -0400

    {add} añadiendo el objetos people para las pruebas

--- IntroduccionFuncional/Program.cs
+++ IntroduccionFuncional/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace IntroduccionFuncional
 {
     class Program
     {
+
+        public class People { 
+            public string Name { get; set; }
+            public string Country { get; set; }
+            public bool isDrunk { get; set; }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var peoples = new List<People>()
+            {
+                
+                //chuck descubre el calculo lamda y turing apoya 
+                //en muchos lenguajes de programacion los paraigmas conviven con otros como ser poo o imperativo
+                //ejemplos Java, C, javascript
+                new People(){ Name = "christoper", Country = "Mexico", isDrunk = true}
+            };
+            
         }
     }
 }
